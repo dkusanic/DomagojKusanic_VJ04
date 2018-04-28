8ff5114ed3c6b66ffe0bb84c56b21422bd77419b
dkusanic <dkusanic@foi.hr>
2018-04-28 09:24:37 +0200

    debug

--- LOTO aplikacija/LOTO aplikacija/Loto.cs
+++ LOTO aplikacija/LOTO aplikacija/Loto.cs
@@ -19,70 +19,70 @@ namespace LOTO_aplikacija
             UplaceniBrojevi = new List<int>();
             DobitniBrojevi = new List<int>();
         }
-    }
-    /// <summary>
-    /// Dodaje korisničke vrijednosti u listu uplaćenih brojeva. Svaka vrijednost se
-    /// prethodno provjerava i uzima u obzir samo ukoliko se radi o cjelobrojnoj
-    /// vrijednosti u rasponu od 1 do 39, i ako već ne postoji ista vrijednost u listi.
-    /// </summary>
-    /// <param name="korisnickeVrijednosti">Vrijednosti koje je unio korisnik.</param>
-    /// <returns>True ako se u listi nalazi 7 jedinstvenih brojeva u rasponu od 1 do
-    /// 39, u suprotnom False.</returns>
-    public bool UnesiUplaceneBrojeve(List<string> korisnickeVrijednosti)
-    {
-        bool ispravni = false;
-        UplaceniBrojevi.Clear();
-        foreach (string v in korisnickeVrijednosti)
+        /// <summary>
+        /// Dodaje korisničke vrijednosti u listu uplaćenih brojeva. Svaka vrijednost se
+        /// prethodno provjerava i uzima u obzir samo ukoliko se radi o cjelobrojnoj
+        /// vrijednosti u rasponu od 1 do 39, i ako već ne postoji ista vrijednost u listi.
+        /// </summary>
+        /// <param name="korisnickeVrijednosti">Vrijednosti koje je unio korisnik.</param>
+        /// <returns>True ako se u listi nalazi 7 jedinstvenih brojeva u rasponu od 1 do
+        /// 39, u suprotnom False.</returns>
+        public bool UnesiUplaceneBrojeve(List<string> korisnickeVrijednosti)
         {
-            int broj = 0;
-            if (int.TryParse(v, out broj) == true)
+            bool ispravni = false;
+            UplaceniBrojevi.Clear();
+            foreach (string v in korisnickeVrijednosti)
             {
-                if (broj >= 1 && broj <= 39)
+                int broj = 0;
+                if (int.TryParse(v, out broj) == true)
                 {
-                    if (UplaceniBrojevi.Contains(broj) == false)
+                    if (broj >= 1 && broj <= 39)
                     {
-                        UplaceniBrojevi.Add(broj);
+                        if (UplaceniBrojevi.Contains(broj) == false)
+                        {
+                            UplaceniBrojevi.Add(broj);
+                        }
                     }
                 }
             }
-        }
-        if (UplaceniBrojevi.Count == 7)
-        {
-            ispravni = true;
-        }
-        return ispravni;
-    }
-    /// <summary>
-    /// Generira 7 nasumičnih jedinstvenih cijelih brojeva u rasponu od 1 do 39,
-    /// te ih sprema u listu kao dobitnu kombinaciju brojeva.
-    /// </summary>
-    public void GenerirajDobitnuKombinaciju()
-    {
-        DobitniBrojevi.Clear();
-        Random generatorBrojeva = new Random();
-        while (DobitniBrojevi.Count < 7)
-        {
-            int broj = generatorBrojeva.Next(1, 39);
-            if (DobitniBrojevi.Contains(broj) == false)
+            if (UplaceniBrojevi.Count == 7)
             {
-                DobitniBrojevi.Add(broj);
+                ispravni = true;
+            }
+            return ispravni;
+        }
+        /// <summary>
+        /// Generira 7 nasumičnih jedinstvenih cijelih brojeva u rasponu od 1 do 39,
+        /// te ih sprema u listu kao dobitnu kombinaciju brojeva.
+        /// </summary>
+        public void GenerirajDobitnuKombinaciju()
+        {
+            DobitniBrojevi.Clear();
+            Random generatorBrojeva = new Random();
+            while (DobitniBrojevi.Count < 7)
+            {
+                int broj = generatorBrojeva.Next(1, 39);
+                if (DobitniBrojevi.Contains(broj) == false)
+                {
+                    DobitniBrojevi.Add(broj);
+                }
             }
         }
-    }
-    /// <summary>
-    /// Izračunava broj pogođenih brojeva dobitne kombinacije.
-    /// </summary>
-    /// <returns>Broj pogođenih brojeva dobitne kombinacije.</returns>
-    public int IzracunajBrojPogodaka()
-    {
-        int brojPogodaka = 0;
-        foreach (int broj in UplaceniBrojevi)
+        /// <summary>
+        /// Izračunava broj pogođenih brojeva dobitne kombinacije.
+        /// </summary>
+        /// <returns>Broj pogođenih brojeva dobitne kombinacije.</returns>
+        public int IzracunajBrojPogodaka()
         {
-            if (DobitniBrojevi.Contains(broj) == true)
+            int brojPogodaka = 0;
+            foreach (int broj in UplaceniBrojevi)
             {
-                brojPogodaka++;
+                if (DobitniBrojevi.Contains(broj) == true)
+                {
+                    brojPogodaka++;
+                }
             }
+            return brojPogodaka;
         }
-        return brojPogodaka;
     }
 }
